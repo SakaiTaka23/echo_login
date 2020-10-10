ef1c7ddfa0eb1ed80b7411fcd53d9b3cfbf7e2c8
SakaiTaka23 <tt3.sakai@gmail.com>
2020-10-10 14:39:27 +0900

    ルーティングを一つにまとめた

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"github.com/SakaiTaka23/echo_login/handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
 func main() {
-    router := newRouter()
-    router.Logger.Fatal(router.Start(":8000"))
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Static("/assets", "public/assets")
+
+	e.File("/", "public/index.html")        // GET /
+	e.File("/signup", "public/signup.html") // GET /signup
+	e.POST("/signup", handler.Signup)       // POST /signup
+	e.File("/login", "public/login.html")   // GET /login
+	e.POST("/login", handler.Login)         // POST /login
+	e.File("/todos", "public/todos.html")   // GET /todos
+
+	api := e.Group("/api")
+	api.Use(middleware.JWTWithConfig(handler.Config))   // /api 下はJWTの認証が必要
+	api.GET("/todos", handler.GetTodos)                 // GET /api/todos
+	api.POST("/todos", handler.AddTodo)                 // POST /api/todos
+	api.DELETE("/todos/:id", handler.DeleteTodo)        // DELETE /api/todos/:id
+	api.PUT("/todos/:id/completed", handler.UpdateTodo) // PUT /api/todos/:id/completed
+
+	e.Logger.Fatal(e.Start(":8000"))
 }
